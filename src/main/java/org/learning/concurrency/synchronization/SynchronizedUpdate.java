54277dd77654048c1a3a57b003b7c25c8296f191
Abdullah Baig <abdullahbaig456@gmail.com>
2020-06-15 23:11:12 +0200

    Add synchronizated increment sample

---
+++ src/main/java/org/learning/concurrency/synchronization/SynchronizedUpdate.java
@@ -0,0 +1,30 @@
+package org.learning.concurrency.synchronization;
+
+public class SynchronizedUpdate {
+
+    public static void main(String[] args) throws InterruptedException {
+        Counter c = new Counter();
+        int REPEAT = 10_000_000;
+        Runnable r = () -> {
+            for (int i=0; i<REPEAT; i++) {
+                c.increment();
+            }
+        };
+
+        Thread t1 = new Thread(r);
+        Thread t2 = new Thread(r);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        System.out.println(c.i);
+    }
+
+    public static class Counter {
+        private int i = 0;
+        public synchronized int increment() {
+            return i++;
+        }
+    }
+}
